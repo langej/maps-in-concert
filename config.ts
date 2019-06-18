8e7a0eab9ccf6796987548ee7577cfe8002dd47f
langej <langej@users.noreply.github.com>
2019-06-18 19:49:36 +0200

    Create empty config.ts

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const PORT = 8080
+
+// Songkick => https://www.songkick.com/api_key_requests/new
+export const SONGKICK_API_KEY = ""
+
+// SetlistFM => https://api.setlist.fm/docs/1.0/index.html
+export const SETLISTFM_API_KEY = ""
+
+// HERE Maps => https://developer.here.com/products/maps
+export const app_id = ""
+export const app_code = ""
